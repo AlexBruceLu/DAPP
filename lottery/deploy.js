4eee5db97fd70b00910b66fb9fcc716bd7598e84
AlexBruceLu <lu_1000_yu@163.com>
2018-11-01 13:05:13 +0800

    部署到真实测试网络

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -1,41 +1,46 @@
-let Web3 = require('web3');
-let {interface,bytecode} = require('./compile');
-let web3 = new Web3();
-web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
+var Web3 = require('web3');
+var {interface,bytecode} = require('./compile');
+// var web3 = new Web3();
+// web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545')); //私有网络部署
 
-// deploy = async () => {
-// 	try{
-// 		accounts = await web3.eth.getAccounts();
-// 		contractInstance = await new web3.eth.Contract(JSON.parse(interface)).deploy(
-// 			{
-// 				data: bytecode,
-// 			}
-// 		).send({
-// 			from: accounts[0],
-// 			gas: '1000000',
-// 		})
-// 		console.log('address: ',contractInstance.options.address);
-// 	}catch(e){
-// 		console.log(e);
-// 	}
-// }
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var mnemonic = 'sting pig good leg claw witness switch sibling assume surge segment hawk';
+var provider = new HDWalletProvider(mnemonic,'https://ropsten.infura.io/v3/0c5480ee2dca44e98ad89ecd2bc0288d');
+var web3  = new Web3(provider);
+
+deploy = async () => {
+	try{
+		// accounts = await web3.eth.getAccounts();
+		contractInstance = await new web3.eth.Contract(JSON.parse(interface)).deploy(
+			{
+				data: bytecode,
+			}
+		).send({
+			from: '0x8427D31B355F808887eE05743FC13Fecaba0DB93',
+			gas: 1000000,
+		})
+		console.log('address: ',contractInstance.options.address);
+	}catch(e){
+		console.log(e);
+	}
+}
 
 // deploy();
 
-console.log('version: ',web3.version);
-console.log('provider: ',web3.currentProvider);
-deploy = async () => {
-	var contract = await new web3.eth.Contract(JSON.parse(interface));
-	// console.log('1111',contract);
-	var contractInstance = await contract.deploy({
-		data: bytecode,
-		// arguments: ['helloWorld'],
-	}).send({
-		from: '0xf500e377400a8ce9c4789cd10a87ca2d265d3c56',
-		gas: 1000000,	
-	});
-	// console.log('222222222222222222222222222222   ',contractInstance);
-	console.log('address: ',contractInstance.options.address);
-}
+// console.log('version: ',web3.version);
+// console.log('provider: ',web3.currentProvider);
+// deploy = async () => {
+// 	var contract = await new web3.eth.Contract(JSON.parse(interface));
+// 	// console.log('1111',contract);
+// 	var contractInstance = await contract.deploy({
+// 		data: bytecode,
+// 		// arguments: ['helloWorld'],
+// 	}).send({
+// 		from: '0x7c81f821d13f3db90d9dd36d15b419b4e8f23063',
+// 		gas: 1000000,	
+// 	});
+// 	// console.log('222222222222222222222222222222   ',contractInstance);
+// 	console.log('address: ',contractInstance.options.address);
+// }
 
 deploy();
